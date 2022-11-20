119d5577678104847b469db9d3448d9b5eb736b7
Matt <64214044+kvbc@users.noreply.github.com>
2022-11-20 12:12:19 +0100

    Add files via upload

--- Scripts/HUD/Minimap.gd
+++ Scripts/HUD/Minimap.gd
@@ -1,22 +1,47 @@
 extends Control
 
-func update_rooms (focus_room):
-	for room in ALMain.idx_map:
-		var txt = $ColorRect.get_child(room.idx)
-		txt.rect_position = $ColorRect.rect_size / 2 + Vector2(room.pos.x - focus_room.pos.x, room.pos.y - focus_room.pos.y) * txt.rect_size - txt.rect_size / 2
+var is_inited = false
+
+const SIZE = Vector2(8, 8)
+
+func get_room_draw_positions (room, pos, visited = {}):
+	if visited.has(room):
+		return visited
+	visited[room] = pos
+	
+	for chunk_pos in ALMain.room_chunks[room.type]:
+		if room.HasChunk(chunk_pos):
+			var chunk = room.GetChunk(chunk_pos)
+			for dir in ALMain.room_chunk_exit_dirs[room.type][chunk_pos]:
+				if chunk.HasNeighbour(dir):
+					var neighbour_chunk = chunk.GetNeighbour(dir)
+					var neighbour_room = neighbour_chunk.GetRoom()
+					var new_pos = pos + chunk_pos * ALMain.GetRoomTilesPerChunk() * SIZE
+					new_pos += ALMain.DirVector(dir) * ALMain.GetRoomTilesPerChunk() * SIZE
+					new_pos -= neighbour_chunk.pos * ALMain.GetRoomTilesPerChunk() * SIZE
+					visited = get_room_draw_positions(neighbour_room, new_pos, visited)
+	return visited
+
+func _draw ():
+	if not is_inited:
+		return
+	var visited = get_room_draw_positions(ALMain.map, Vector2.ZERO)
+	for room in visited.keys():
+		var pos = visited[room]
+		pos += $ColorRect.rect_size / 2 # center the container
+		pos -= ALMain.room_rects[ALMain.current_room.type].size / 2 * SIZE # center the room
+		pos -= visited[ALMain.current_room] # center all rooms
+		for wall_pos in ALMain.room_wall_positions[room.type]:
+			var rect = Rect2(pos + wall_pos * SIZE, SIZE)
+			if room == ALMain.current_room:
+				draw_rect(rect, Color.red)
+			else:
+				draw_rect(rect, Color.white)
 
 func Init ():
-	for room in ALMain.idx_map:
-		var txt = TextureRect.new()
-		txt.expand = true
-		txt.texture = preload("res://icon.png")
-		txt.rect_size = Vector2(64, 64)
-		$ColorRect.add_child(txt)
-	update_rooms(ALMain.map)
-		
-func MarkRoomInactive (room):
-	$ColorRect.get_child(room.idx).self_modulate = Color.white
-		
-func MarkRoomActive (room):
-	$ColorRect.get_child(room.idx).self_modulate = Color.red
-	update_rooms(room)
+	is_inited = true
+	update()
+	pass
+	
+func Update ():
+	update()
--- Scripts/Room.gd
+++ Scripts/Room.gd
@@ -1,28 +1,60 @@
 extends Node2D
 
-var dir_exit = {}
-var possible_exit_directions = []
+var chunk_dir_exit_positions = {} # chunk pos -> dir -> exit position
+var empty_cells = []
+var min_empty_cell
+var room : ALMain.MapRoom
 
-func on_exit_entered (body, method):
+func on_exit_entered (body, method, chunk : ALMain.RoomChunk):
 	if ALMain.IsBodyPlayer(body):
-		ALMain.call(method)
+		ALMain.call(method, chunk)
+
+func tile_to_chunk_pos (tile_pos):
+	var chunk_tiles = ALMain.GetRoomTilesPerChunk()
+	return ((tile_pos - min_empty_cell) / Vector2(chunk_tiles, chunk_tiles)).floor()
+
+func get_grouped_tilemap_rects (tilemap):
+	var rects = []
+	var groups = ALUtil.GroupTilemapTiles(tilemap)
+	for group in groups:
+		var min_pos = null
+		var max_pos = null
+		for pos in group:
+			if min_pos == null:
+				min_pos = pos
+			else:
+				min_pos.x = min(min_pos.x, pos.x)
+				min_pos.y = min(min_pos.y, pos.y)
+			if max_pos == null:
+				max_pos = pos
+			else:
+				max_pos.x = max(max_pos.x, pos.x)
+				max_pos.y = max(max_pos.y, pos.y)
+		var rect = Rect2(min_pos, max_pos - min_pos + Vector2(1, 1))
+		rects.append(rect)
+	return rects
 
 func create_exit (dir, tilemap, move_method):
-	var rect = tilemap.get_used_rect()
-	rect.size *= tilemap.cell_size
-	rect.position *= tilemap.cell_size
+	for rect in get_grouped_tilemap_rects(tilemap):
+		var tile_pos_in_front = rect.position - ALMain.DirVector(dir) * 2
+		var chunk_pos_in_front = tile_to_chunk_pos(tile_pos_in_front)
+		if not chunk_pos_in_front in chunk_dir_exit_positions:
+			chunk_dir_exit_positions[chunk_pos_in_front] = {}
+		chunk_dir_exit_positions[chunk_pos_in_front][dir] = tile_pos_in_front * tilemap.cell_size
+		
+		var chunk = room.GetChunk(chunk_pos_in_front)
+		if chunk.HasNeighbour(dir):
+			rect.size *= tilemap.cell_size
+			rect.position *= tilemap.cell_size
+			var exit = Area2D.new()
+			exit.position = rect.position + rect.size / 2
+			var cs = CollisionShape2D.new()
+			cs.shape = RectangleShape2D.new()
+			cs.shape.extents = rect.size / 2
+			exit.add_child(cs)
+			$Exits.add_child(exit)
+			exit.connect("body_entered", self, "on_exit_entered", [move_method, chunk])
 	
-	var exit = Area2D.new()
-	exit.position = rect.position + rect.size / 2
-	exit.name = tilemap.name
-	var cs = CollisionShape2D.new()
-	cs.shape = RectangleShape2D.new()
-	cs.shape.extents = rect.size / 2
-	exit.add_child(cs)
-	$Exits.add_child(exit)
-	exit.connect("body_entered", self, "on_exit_entered", [move_method])
-	dir_exit[dir] = exit
-
 func has_exit (dir):
 	if (dir == ALMain.Dir.LEFT ): return not $Exit_Tilemaps/Left .get_used_cells().empty()
 	if (dir == ALMain.Dir.RIGHT): return not $Exit_Tilemaps/Right.get_used_cells().empty()
@@ -30,9 +62,24 @@ func has_exit (dir):
 	if (dir == ALMain.Dir.DOWN ): return not $Exit_Tilemaps/Down .get_used_cells().empty()
 
 func _ready ():
-	for dir in [ALMain.Dir.LEFT, ALMain.Dir.RIGHT, ALMain.Dir.UP, ALMain.Dir.DOWN]:
-		if has_exit(dir):
-			possible_exit_directions.append(dir)
+	var rect = $TileMap.get_used_rect()
+	for x in range(rect.position.x, rect.size.x):
+		var empty = false
+		for y in range(rect.position.y, rect.size.y):
+			var pos = Vector2(x, y)
+			if $Exit_Tilemaps/Left .get_used_cells().has(pos): continue
+			if $Exit_Tilemaps/Right.get_used_cells().has(pos): continue
+			if $Exit_Tilemaps/Up   .get_used_cells().has(pos): continue
+			if $Exit_Tilemaps/Down .get_used_cells().has(pos): continue
+			if $TileMap.get_used_cells().has(pos):
+				empty = not empty
+			elif empty:
+				empty_cells.append(pos)
+				if min_empty_cell == null:
+					min_empty_cell = pos
+				else:
+					min_empty_cell.x = min(min_empty_cell.x, x)
+					min_empty_cell.y = min(min_empty_cell.y, y)
 
 func SpawnBullet (pos:Vector2, velocity:Vector2, damage:int):
 	var bullet = preload("res://Scenes/Bullet.tscn").instance()
@@ -40,19 +87,60 @@ func SpawnBullet (pos:Vector2, velocity:Vector2, damage:int):
 	bullet.Init(velocity, damage)
 	$Bullets.add_child(bullet)
 
-func GetPossibleExitDirections ():
-	return possible_exit_directions
+func ToChunkPosition (pos):
+	return tile_to_chunk_pos(pos / $TileMap.cell_size)
 
-func GetExitPosition (dir):
-	return dir_exit[dir].global_position
+func GetPossibleExitDirs ():
+	var dirs = []
+	for dir in ALMain.GetAllDirs():
+		if has_exit(dir):
+			dirs.append(dir)
+	return dirs
 
-func InitRoom (
-	is_neighbour_on_left  : bool,
-	is_neighbour_on_right : bool,
-	is_neighbour_up       : bool,
-	is_neighbour_down     : bool,
+func GetPossibleChunkExitDirs ():
+	var chunk_exit_dirs = {}
+	for chunk_pos in GetChunks():
+		chunk_exit_dirs[chunk_pos] = []
+	for dir in ALMain.GetAllDirs():
+		if has_exit(dir):
+			var tilemap = {
+				ALMain.Dir.LEFT : $Exit_Tilemaps/Left,
+				ALMain.Dir.RIGHT: $Exit_Tilemaps/Right,
+				ALMain.Dir.UP   : $Exit_Tilemaps/Up,
+				ALMain.Dir.DOWN : $Exit_Tilemaps/Down
+			}[dir]
+			for exit_rect in get_grouped_tilemap_rects(tilemap):
+				var tile_pos = exit_rect.position - ALMain.DirVector(dir)
+				var chunk_pos = tile_to_chunk_pos(tile_pos)
+				chunk_exit_dirs[chunk_pos].append(dir)
+	return chunk_exit_dirs
+
+func GetExitPosition (chunk_pos, dir):
+	return chunk_dir_exit_positions[chunk_pos][dir]
+
+func GetWallPositions ():
+	return $TileMap.get_used_cells()
+
+func GetRect ():
+	return $TileMap.get_used_rect()
+
+func GetInsideSize ():
+	return $TileMap.get_used_rect().size - Vector2(2, 2)
+
+func GetChunks ():
+	var chunks = []
+	for pos in empty_cells:
+		var chunk_pos = tile_to_chunk_pos(pos)
+		if not chunks.has(chunk_pos):
+			chunks.append(chunk_pos)
+	return chunks
+
+func Init (
+	_room : ALMain.MapRoom,
 	entered_from_dir
 ):
+	room = _room
+	
 	var bullets = Node2D.new()
 	bullets.name = "Bullets"
 	add_child(bullets)
@@ -65,10 +153,14 @@ func InitRoom (
 	enemies.name = "Enemies"
 	add_child(enemies)
 	
-	if (is_neighbour_on_left  and has_exit(ALMain.Dir.LEFT )): create_exit(ALMain.Dir.LEFT,  $Exit_Tilemaps/Left,  "MoveLeft")
-	if (is_neighbour_on_right and has_exit(ALMain.Dir.RIGHT)): create_exit(ALMain.Dir.RIGHT, $Exit_Tilemaps/Right, "MoveRight")
-	if (is_neighbour_up       and has_exit(ALMain.Dir.UP   )): create_exit(ALMain.Dir.UP,    $Exit_Tilemaps/Up,    "MoveTop")
-	if (is_neighbour_down     and has_exit(ALMain.Dir.DOWN )): create_exit(ALMain.Dir.DOWN,  $Exit_Tilemaps/Down,  "MoveBottom")
+#	if has_exit(ALMain.Dir.LEFT ): create_exit(ALMain.Dir.LEFT,  $Exit_Tilemaps/Left,  "MoveLeft")
+#	if has_exit(ALMain.Dir.RIGHT): create_exit(ALMain.Dir.RIGHT, $Exit_Tilemaps/Right, "MoveRight")
+#	if has_exit(ALMain.Dir.UP   ): create_exit(ALMain.Dir.UP,    $Exit_Tilemaps/Up,    "MoveTop")
+#	if has_exit(ALMain.Dir.DOWN ): create_exit(ALMain.Dir.DOWN,  $Exit_Tilemaps/Down,  "MoveBottom")
+	create_exit(ALMain.Dir.LEFT,  $Exit_Tilemaps/Left,  "MoveLeft")
+	create_exit(ALMain.Dir.RIGHT, $Exit_Tilemaps/Right, "MoveRight")
+	create_exit(ALMain.Dir.UP,    $Exit_Tilemaps/Up,    "MoveTop")
+	create_exit(ALMain.Dir.DOWN,  $Exit_Tilemaps/Down,  "MoveBottom")
 	
 	if entered_from_dir != null:
 		if has_node("EnemySpawns"):
